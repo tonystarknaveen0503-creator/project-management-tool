dab6fe74b22181850be31374754f6c81e124be98
tonystarknaveen0503-creator <naveen2002jk@gmail.com>
2026-03-03 14:34:36 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Dashboard from "./pages/Dashboard";
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
-  return <Dashboard />;
+  return (
+    <Router basename="/project-management-tool">
+      <Routes>
+        <Route path="/" index exact element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
